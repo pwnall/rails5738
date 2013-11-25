58de9c620b88e329cb3577bcc9eacf0ebacb85c7
Victor Costan <costan@gmail.com>
2013-11-25 11:19:42 -0500

    rails new rails5738 --edge --database=mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails5738::Application.config.secret_key_base = '2d756639ea778579d9b55d3d7852f9130b729a86a42418304f853a4f4ce7502d7a3a0edd6b2dd9097bb01d45b0f4654675abd9bd85283808bf0cb46b8aef633a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails5738::Application.config.session_store :cookie_store, key: '_rails5738_session'
